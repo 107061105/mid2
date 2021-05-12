81aa67e8e4dd8e6703a5fd97eab0eadb4c4815ca
107061105 <yc95086@gmail.com>
2021-05-12 03:19:23 -0700

    add

--- print.py
+++ print.py
@@ -7,8 +7,8 @@ serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
 for k in range(0, 10):
     a = s.readline()
-    gestureID = (int)a
-    if (gestureID == 0)
+    gestureID = int(a)
+    if (gestureID == 0):
         print("gestureID = RING:\n\r"
         "          *       \n\r"
         "       *     *    \n\r"
@@ -17,7 +17,7 @@ for k in range(0, 10):
         "     *         *  \n\r"
         "       *     *    \n\r"
         "          *       \n\r")
-    else if (gestureID == 1)
+    elif (gestureID == 0):
         print("gestureID = SLOPE:\n\r"
         "        *        \n\r"
         "       *         \n\r"
@@ -27,7 +27,7 @@ for k in range(0, 10):
         "   *             \n\r"
         "  *              \n\r"
         " * * * * * * * * \n\r")
-    else if (gestureID == 2)
+    elif (gestureID == 0):
         print("gestureID = DOWN:\n\r"
         "        *        \n\r"
         "        *        \n\r"
@@ -37,10 +37,11 @@ for k in range(0, 10):
         "      * * *      \n\r"
         "       ***       \n\r"
         "        *        \n\r")
-    print("\n\nSequence Number = %d", k)
+    print("\n\nSequence Number = ", k, "\n")
     a = s.readline()
-    num = (int)num
-    for x in range(0, num)
+    num = int(a)
+    y = np.arange(0,num, 1)
+    for x in range(0, num):
         line=s.readline() # Read an echo string from B_L4S5I_IOT01A terminated with '\n'
         y[x] = float(line)
     t = np.arange(0,num, 1)
